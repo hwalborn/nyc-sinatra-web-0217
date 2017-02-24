254947bcfec44935ea677b018cb5c1b49996614c
hwalborn <hwalborn@gmail.com>
2017-02-23 22:00:49 -0500

    Touristy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < Sinatra::Base
 
   set :views, Proc.new { File.join(root, "../views/") }
   register Sinatra::Twitter::Bootstrap::Assets
+
+  patch '/figures/:id' do
+    @figure = Figure.update(params[:id], params[:figure])
+    @figure.landmarks << Landmark.create(params[:landmark])
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.update(params[:id], params[:landmark])
+    redirect to "/landmarks/#{@landmark.id}"
+  end
 end
